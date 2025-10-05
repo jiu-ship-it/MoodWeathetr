2d7ccede37c7fb16a55d78f0c6aea5fa4605e095
LiuDaiJun07 <3192379586@qq.com>
2025-10-05 11:25:07 +0800

    make edit pages

--- uniapp_vue_main/pages/Setting/Setting.vue
+++ uniapp_vue_main/pages/Setting/Setting.vue
@@ -1,64 +1,64 @@
 <template>
-	<div class="page flex-col section">
-		<div class="halfColor">
+	<view class="page flex-col section">
+		<view class="halfColor">
 			<view class="flex-col justify-start items-start text-wrapper pos"><text class="text">暖言纸条</text></view>
 			
-			<!-- 这里是顶部菜单 -->
+			<!-- 这里是底部菜单 -->
 			<view class="flex-col justify-start section_3 pos_4">
-				<view class="flex-row justify-between equal-division">
-					<view class="flex-col justify-start items-center equal-division-item">
+				<view class="flex-row justify-between equal-viewision">
+					<view class="flex-col justify-start items-center equal-viewision-item">
 						<image class="image" src="/static/image/home.png" />
 					</view>
-					<view class="flex-col justify-start items-center equal-division-item">
+					<view class="flex-col justify-start items-center equal-viewision-item">
 						<image class="image" src="/static/image/setting.png" />
 					</view>
-					<view class="flex-col justify-start items-center equal-division-item">
+					<view class="flex-col justify-start items-center equal-viewision-item">
 						<image class="image" src="/static/image/music.png" />
 					</view>
 				</view>
 			</view>
 			
-			<!-- 这里是底部菜单 -->
-			<div class="jumbotron jumbotron-fluid jumbotron_pos EditData">
-			  <div class="container">
-			    <form>
-			      <div class="form-group">
-			        <label for="exampleInputEmail1">新昵称(每月修改一次)</label>
-			        <input class="form-control"><!-- 名称修改 -->
-			      </div>
-			      <div class="form-group">
-			        <label for="exampleInputPassword1">个人简介(50字)</label>
-			        <input class="form-control">
-			      </div>
-			      <button type="submit" class="btn btn-primary">提交</button>
-			    </form>
-			  </div>
-			</div>
-			
 			<view><!-- 设置列表 -->
-				<div class="list-group listPos">
-				  <a href="#" class="list-group-item list-group-item-action listColor" aria-current="true">个性化设置</a>
-				  <a href="#" class="list-group-item list-group-item-action listColor">语言设置：中文</a>
-				  <a href="#" class="list-group-item list-group-item-action listColor">您的奖章</a>
-				  <a href="#" class="list-group-item list-group-item-action listColor">隐私条款</a>
-				  <a class="list-group-item list-group-item-action disabled listColor">关于我们</a>
-				</div>
+				<view class="list-group listPos">
+				  <navigator :class="{disabled:isListVisible}" class="list-group-item list-group-item-action listColor" aria-current="true" @click="ProSet">个性化设置</navigator>
+				  <navigator :class="{disabled:isListVisible}" class="list-group-item list-group-item-action listColor">语言设置：中文</navigator>
+				  <navigator :class="{disabled:isListVisible}" class="list-group-item list-group-item-action listColor">您的奖章</navigator>
+				  <navigator :class="{disabled:isListVisible}" class="list-group-item list-group-item-action listColor">隐私条款</navigator>
+				  <navigator :class="{disabled:isListVisible}" class="list-group-item list-group-item-action listColor">关于我们</navigator>
+				</view>
 			</view>
 
 			<view class="ProfitThings ProfitPos"><!-- 个人简介 -->
-				<div class="jumbotron jumbotron-fluid ProfitPos">
-				  <div class="container">
+				<view class="jumbotron jumbotron-fluid ProfitPos">
+				  <view class="container">
 				    <h1 class="display-4">名称：{{MyName}}</h1>
 				    <p class="lead">简介：{{Introduction}}</p>
-				  </div>
-				</div>
+				  </view>
+				</view>
 			</view>
 			<view>
 				<img src="static/image/noon.png" alt="头像" class="ProImag"/>
 			</view>
 			
-		</div>
-	</div>
+			<view class="jumbotron jumbotron-fluid jumbotron_pos" :style="{visibility:isEditData}">
+			  <view class="container">
+			    <form>
+			      <view class="form-group">
+			        <label for="exampleInputEmail1">新昵称(每月修改一次)</label>
+			        <input class="form-control"><!-- 名称修改 -->
+			      </view>
+			      <view class="form-group">
+			        <label for="exampleInputPassword1">个人简介(50字)</label>
+			        <input class="form-control">
+			      </view>
+			      <button type="submit" class="btn btn-primary">提交</button>
+				  <button type="submit" class="CancelButton btn btn-primary" @click="ProEditCancel">取消</button>
+			    </form>
+			  </view>
+			</view>
+			
+		</view>
+	</view>
 </template>
 
 <script>
@@ -67,13 +67,24 @@
 		props: {},
 		data() {
 			return {
-				NowPage:"main",
-				MyName:"ME",
-				Introduction:"helloWorld"
+				NowPage:"main",//当前页面
+				MyName:"ME",//名称
+				Introduction:"helloWorld",//简介
+				isListVisible:false,//是否可使用列表
+				isEditData:"hidden",//编辑隐藏
 			};
 		},
 
-		methods: {},
+		methods: {
+			ProSet(){
+				this.isListVisible = true;
+				this.isEditData = "visible";
+			},
+			ProEditCancel(){
+				this.isListVisible = false;
+				this.isEditData = "hidden";
+			},
+		},
 	};
 </script>
 
@@ -82,10 +93,10 @@
 	.jumbotron_pos{
 		position: absolute;
 		display: flex;
-		top: 10%;
+		top: 1%;
 		right: 50%;
 		transform: translate(50%,45%);
-		height: 35%;
+		height: 50%;
 		width: 80%;
 	}
 	.ProImag{
@@ -122,7 +133,7 @@
 		margin-top: 67.35rpx;
 	}
 	.EditData{
-		visibility: hidden;
+		visibility: visible;
 	}
 	.page {
 		background-color: #d3d3d3;
@@ -196,11 +207,11 @@
 		top: 1529.13rpx;
 	}
 
-	.equal-division {
+	.equal-viewision {
 		margin: 0 32.77rpx;
 	}
 
-	.equal-division-item {
+	.equal-viewision-item {
 		padding: 7.28rpx 0;
 		background-color: #d9d9d980;
 		border-radius: 14.56rpx;
@@ -215,5 +226,8 @@
 	.image {
 		width: 87.38rpx;
 		height: 87.38rpx;
+	}
+	.CancelButton{
+		left: 10%;
 	}
 </style>
